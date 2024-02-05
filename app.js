0e11e22f28a6f9baec79127cf35fab89fe2a4374
komangsuryasedana <komangsuryasedana@mail.com>
2024-02-05 23:53:20 +0800

    fix: create new midleware endpoint

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express')
+const swaggerJson = require('./swagger.json')
 const userRouter = require("./routes/user.routes");
 const itemRouter = require("./routes/item.routes");
 const orderRouter = require("./routes/order.routes");
 
 const app = express();
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use(express.json());
 app.use(morgan('dev'));
+
+//swagger documentation
 
 app.get("/", (req, res) => {
   return res.status(200).json("Ready");
@@ -17,6 +22,7 @@ app.get("/", (req, res) => {
 app.use(userRouter);
 app.use(itemRouter);
 app.use(orderRouter);
+
 
 //! errror handler
 app.use((err, req, res, next) => {
