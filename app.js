15b6b7047c52b0218794410858cc5225bba76074
komangsuryasedana <komangsuryasedana@mail.com>
2024-02-07 20:05:58 +0800

    edit swagger

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const morgan = require('morgan');
-const swaggerUi = require('swagger-ui-express')
-const swaggerJson = require('./swagger.json')
+const swagger = require("./docs/swager");
 const userRouter = require("./routes/user.routes");
 const itemRouter = require("./routes/item.routes");
 const orderRouter = require("./routes/order.routes");
 
 const app = express();
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use(express.json());
 app.use(morgan('dev'));
 
-//swagger documentation
+//api documentation
+app.use(swagger)
 
 app.get("/", (req, res) => {
   return res.status(200).json("Ready");
 });
-
 
 app.use(userRouter);
 app.use(itemRouter);
