05c24ed410e20a587e186c035c4580a12e3ef1bf
komangsuryasedana <komangsuryasedana@mail.com>
2024-02-22 21:55:49 +0800

    fix: app.js

--- app.js
+++ app.js
@@ -28,9 +28,8 @@ app.use(orderRouter);
 
 //! errror handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
   res.status(500).json({
-    status:500,
+    status: 500,
     error: err.message,
   });
 });
