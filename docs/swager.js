dee36afadd3a86fb70b2476d54da0a91ac601d26
berylmalik <berylmalikaveta@gmail.com>
2024-03-19 23:29:13 +0700

    Adding new endpoint /updateAdmin/v1 and swagger file

--- docs/swager.js
+++ docs/swager.js
@@ -1,9 +1,7 @@
-const swaggerUi = require('swagger-ui-express')
-const swaggerJson = require('./swagger.json')
+const swaggerUi = require("swagger-ui-express");
+const swaggerJson = require("./swagger.json");
 const router = require("express").Router();
 
-
 router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
-
 
 module.exports = router;
