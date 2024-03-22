9a576562aa2a453b190d9d1a3f97e5d5c55e8b66
berylmalik <berylmalikaveta@gmail.com>
2024-03-23 01:04:33 +0700

    chore: test deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const swagger = require("./docs/swager");
 const userRouter = require("./routes/user.routes");
 const itemRouter = require("./routes/item.routes");
@@ -10,13 +10,13 @@ const passport = require("passport");
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-//api documentation
-app.use(swagger)
+//API documentation
+app.use(swagger);
 
 app.get("/", (req, res) => {
-  return res.status(200).json("Ready");
+    return res.status(200).json("Ready");
 });
 
 app.use(passport.initialize()); // ini passport
@@ -25,20 +25,19 @@ app.use(userRouter);
 app.use(itemRouter);
 app.use(orderRouter);
 
-
 //! errror handler
 app.use((err, req, res, next) => {
-  res.status(500).json({
-    status: 500,
-    error: err.message,
-  });
+    res.status(500).json({
+        status: 500,
+        error: err.message,
+    });
 });
 
 app.use((req, res) => {
-  res.status(404).json({
-    status: 404,
-    error: "METHODE AND ENDPOINT NOT FOUND!",
-  });
+    res.status(404).json({
+        status: 404,
+        error: "METHODE AND ENDPOINT NOT FOUND!",
+    });
 });
 
 module.exports = app;
